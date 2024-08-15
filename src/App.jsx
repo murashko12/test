e77ae1c5ef319f86a474225120652f15bcca69e5
murashko12 <pert@ocumare.ru>
2024-08-15 17:54:37 +0300

    CompFirst

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CompFirst from './components/CompFirst'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +26,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <CompFirst/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ src/components/CompFirst.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompFirst = () => {
+  return (
+    <div>
+      CompFirst
+    </div>
+  )
+}
+
+export default CompFirst
